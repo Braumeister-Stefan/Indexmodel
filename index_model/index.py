d51f383ee53b175f9afbbe8e2e562885d15a284c
Stephane <smdw1997@gmail.com>
2022-05-16 21:35:08 +0200

    Documented code

--- index_model/index.py
+++ index_model/index.py
@@ -6,31 +6,28 @@ import os.path
 
 class IndexModel:
 
-
     def __init__(self) -> None:
         self.final_index = 0
+
+        # Here I store all constants
+        self.weight_1 = 0.5
+        self.weight_2 = 0.25
+        self.weight_3 = 0.25
+        self.base_index = 100
 
 
     def calc_index_level(self, start_date: dt.date, end_date: dt.date) -> None:
 
-
-
         warnings.filterwarnings(action="ignore")
+        #warning was generated that might be relevant for later resolving,
+        # but it does not impact results.
 
-        # create general objects
-
+        # import the stock data
         df = pd.read_csv(os.path.join("data_sources","stock_prices.csv"), sep=",")
 
         df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
 
-        weight_1 = 0.5
-        weight_2 = 0.25
-        weight_3 = 0.25
-
-
-        base_index = 100
-
-        # select the rebalancing dates
+        # calculate the rebalancing dates
 
         rebalancing_dates = pd.DataFrame(df["Date"])
 
@@ -44,7 +41,7 @@ class IndexModel:
 
         rebalancing_EOM = rebalancing_dates["Date"].loc[rebalancing_dates["Rebalance"] == True]
 
-        # ranking
+        #based on rebalancing dates, rank the top 3 stocks in a table.
 
         ranking_frame = pd.DataFrame(columns=["Date", "#1", "#2", "#3"])
 
@@ -57,7 +54,7 @@ class IndexModel:
             winners1 = order.head(3).index.values
             winners2 = np.append(balancing_date, winners1)
             ranking_frame.loc[len(ranking_frame)] = winners2
-        # task 1, convert ranking frame to weights frame
+        # convert the ranking frame into a weights frame with the weights defined in __init__
 
         weighting_frame = pd.DataFrame(data=rebalancing_EOM, columns=df.columns)
         weighting_frame = weighting_frame.fillna(0)
@@ -68,38 +65,40 @@ class IndexModel:
 
                 if y == ranking_frame["#1"].loc[ranking_frame["Date"] == x].values:
 
-                    weighting_frame[y].loc[weighting_frame["Date"] == x] = weight_1
+                    weighting_frame[y].loc[weighting_frame["Date"] == x] = self.weight_1
 
                 elif y == ranking_frame["#2"].loc[ranking_frame["Date"] == x].values:
 
-                    weighting_frame[y].loc[weighting_frame["Date"] == x] = weight_2
+                    weighting_frame[y].loc[weighting_frame["Date"] == x] = self.weight_2
 
                 elif y == ranking_frame["#3"].loc[ranking_frame["Date"] == x].values:
 
-                    weighting_frame[y].loc[weighting_frame["Date"] == x] = weight_3
+                    weighting_frame[y].loc[weighting_frame["Date"] == x] = self.weight_3
 
         weighting_frame["Date"] = df["Date"].iloc[weighting_frame.index.values + 1].values
 
-        # index starting value
+        # convert the price indices to return indices for simplifying calculations
 
         returns = df.copy()
         returns.iloc[:, 1:] = returns.iloc[:, 1:].pct_change()
 
+        #create an intermediary index with a column per stock. This can later be collapsed to the final index
         index = pd.DataFrame(df["Date"], columns=df.columns)
 
         placeholder = df["Date"].loc[df["Date"] == pd.to_datetime(start_date)].index[
             0]  # retrieve index position where the index should start
 
+        #calculate index values for the first period
         day_weight = weighting_frame.loc[weighting_frame["Date"] == pd.to_datetime(start_date)].reset_index(
             drop=True)  # weight for first date
         day_weight_abs = day_weight.copy()
-        day_weight_abs.iloc[:, 1:] = base_index * day_weight.iloc[:, 1:]
+        day_weight_abs.iloc[:, 1:] = self.base_index * day_weight.iloc[:, 1:]
 
         first_index = (day_weight_abs.iloc[:, 1:])
 
         index.iloc[placeholder, 1:] = first_index.values[0]
 
-        # next periods
+        # using a loop, calculate the index values for all subsequent periods
 
         for x in range((placeholder + 1), len(index)):
 
@@ -116,9 +115,7 @@ class IndexModel:
             else:
                 index.iloc[x, 1:] = day_index.values[0]
 
-
-
-        # create final index
+        #collapse the intermediary index to create the final index
 
         final_index = pd.DataFrame(df["Date"], columns=["Date", "final_index"])
         final_index["final_index"] = index.iloc[:, 1:].sum(axis=1)
@@ -126,11 +123,8 @@ class IndexModel:
         final_index = final_index.loc[final_index["Date"] >= pd.to_datetime(start_date), :]
         final_index = final_index.loc[final_index["Date"] <= pd.to_datetime(end_date), :]
 
+        #store the index so the function below can export it
         self.final_index = final_index
-
-
-
-
 
     def export_values(self, file_name: str) -> None:
 
